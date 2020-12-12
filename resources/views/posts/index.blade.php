716025bc6423226bedf230af43a739b3ef4506f1
EC2 Default User <ec2-user@ip-172-31-44-37.us-east-2.compute.internal>
2020-12-12 12:37:26 +0000

    first commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts')
+
+@section('content')
+
+  
+  <div class="container mt-4">
+    <div class="mb-4">
+      <a href="{{route('posts.create')}}" class="btn btn-primary">
+        投稿を新規作成する
+      </a>
+    </div>
+      <div class="card mt-4">
+        @foreach ($posts as $post)
+          <div class="card-header mb-2">
+              {{ $post->title }}
+          </div>
+          <div class="card-body">
+              <p class="card-text">
+                  {{ $post->body }}
+              </p>
+              
+          </div>
+          <div class="card-footer">
+              <span class="mr-2">
+                  投稿日時
+              </span>
+          </div>
+      @endforeach
+      </div>
+  </div>
+  
+@endsection
